795f8565f29b8d822b5e8b17a229abed96f97497
Manohar Reddy K R <manoharreddy.ketham@gmail.com>
2022-02-08 14:07:53 -0500

    Food-Truck-First Commit

---
+++ src/main/java/com/truck/food/FoodTruckSvcApplication.java
@@ -0,0 +1,13 @@
+package com.truck.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTruckSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTruckSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/truck/food/FoodTruckSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truck.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTruckSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
